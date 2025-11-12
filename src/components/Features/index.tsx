4a326409e4081933b87b6b31d4e7028323d3e121
stewebb <xiaoancloud@outlook.com>
2025-11-12 14:14:13 +1100

    Update features data and section title for improved service representation

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -8,8 +8,8 @@ const Features = () => {
       <section id="features" className="py-16 md:py-20 lg:py-28">
         <div className="container">
           <SectionTitle
-            title="Main Features"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            title="Services"
+            paragraph="Apart from Eventies EMS, Snowsy offers a range of services to help your business succeed in the digital age."
             center
           />
 
